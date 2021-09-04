85081845a4c0bf5581374243a68838d5004ab4cc
Soyeon Yang <48446896+Noeyso@users.noreply.github.com>
2021-09-04 13:01:33 +0900

    Create 수열의합.py

---
+++ BOJ/수학/수열의합.py
@@ -0,0 +1,19 @@
+##백준_1024
+
+N,L = map(int,input().split())
+
+t=0
+x=-1
+it=0
+
+for i in range(L,101):
+    t=(i*i-i)/2
+    if (N-t)%i==0 and (N-t)//i>=0:
+        x=(N-t)//i
+        it=i
+        break
+if x==-1:
+    print(-1)
+else:
+    for i in range(it):
+        print(int(x+i),end=' ')
