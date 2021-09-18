51e29752a7de0332ef77885946e4355e7614a83b
Soyeon Yang <48446896+Noeyso@users.noreply.github.com>
2021-09-18 21:49:56 +0900

    Create 트리순회.py

---
+++ BOJ/재귀/트리순회.py
@@ -0,0 +1,36 @@
+## 백준_1991
+
+from sys import stdin
+input = stdin.readline
+
+N =int(input())
+tree={}
+for i in range(N):
+    root,l,r = input().rsplit()
+    tree[root]=[l,r]
+
+def preorder(root):
+    if root!='.':
+        print(root,end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+        
+            
+def inorder(root):
+    if root!='.':
+        inorder(tree[root][0])
+        print(root,end='')
+        inorder(tree[root][1])
+
+def postorder(root):
+    if root!='.':
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root,end='')
+
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
