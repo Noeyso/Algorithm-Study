c703ef88f3226eb37c03c6820ce51b483d09de65
Soyeon Yang <48446896+Noeyso@users.noreply.github.com>
2021-10-11 11:10:00 +0900

    Create 정수삼각형.py

---
+++ Programmers/DP/정수삼각형.py
@@ -0,0 +1,11 @@
+def solution(triangle):
+    leng=len(triangle)
+    dp=[[0 for _ in range(len(triangle[i]))]for i in range(leng)]
+    dp[0][0]=triangle[0][0]
+    for i in range(leng-1):
+        for j in range(len(triangle[i])):
+            dp[i+1][j]=max(dp[i+1][j],dp[i][j]+triangle[i+1][j])
+            dp[i+1][j+1]=max(dp[i+1][j+1],dp[i][j]+triangle[i+1][j+1])
+    return max(dp[leng-1])
+
+print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
