7ab617f02d637621e1caece3a1a2c57a6d075538
Soyeon Yang <48446896+Noeyso@users.noreply.github.com>
2022-06-11 14:07:53 +0900

    Update TwoSum.js

--- LeetCode/TwoSum.js
+++ LeetCode/TwoSum.js
@@ -1,5 +1,6 @@
 var twoSum = function(nums, target) {
-    /*
+    /* 
+    시간복잡도 최대 O(n**2)
     for(let i=1;i<nums.length;i++){
        for(let j=0;j<i;j++){
             if(nums[j]+nums[i]==target){
@@ -8,6 +9,8 @@ var twoSum = function(nums, target) {
         }
     }
     */
+    
+    // 시간복잡도 최대 O(N)
     let numsObj={};
     for(let i=0;i<nums.length;i++){
         let current = nums[i];
